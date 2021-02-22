c3783238d59d3857ed7d6e302091b287525b48b4
Rui Oliveira <rpoliveira02@gmail.com>
2021-02-22 12:52:02 +0000

    Added move generation header

---
+++ src/include/movegeneration.h
@@ -0,0 +1,26 @@
+#ifndef MOVEGENERATION_H_
+#define MOVEGENERATION_H_
+
+
+#include "position.h"
+
+
+
+std::vector<Move> generateAllMoves(Position);
+std::vector<Move> generateAllPieceMoves(Position,Piece,Color);
+std::vector<Move> generateKingMoves(Position,Square);
+std::vector<Move> generatePawnMoves(Position,Square);
+std::vector<Move> generateKnightMoves(Position,Square);
+std::vector<Move> generateBishopMoves(Position,Square);
+std::vector<Move> generateRookMoves(Position,Square);
+std::vector<Move> generateQueenMoves(Position,Square);
+
+
+std::vector<Move> generateCastling(Position, Square);
+std::vector<Move> generatePromotions(Position, Square);
+std::vector<Move> generateEnPassant(Position, Square);
+
+
+bool inCheck(Position,Color);
+
+#endif // MOVEGENERATION_H_
